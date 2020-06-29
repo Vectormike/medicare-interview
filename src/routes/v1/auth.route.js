71ae8bc9257d8a51ecb5a3ed8b74b07461376d90
Redeemer40 <victorjonah199@gmail.com>
2020-06-29 08:54:22 +0100

    add route for auth

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -6,6 +6,7 @@ const authController = require('../../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/register', validate(authValidation.register), authController.register);
+router.post('/registerOrg', authController.registerOrganization);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
 
